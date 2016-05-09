a28b9d3458aa8077c1f2c265916c5d7aee321c11
Dave Borncamp <dborncamp@stsci.edu>
2016-05-09 18:20:44 -0400

    Adding in Ben and Whinie pieces

--- Final_project.py
+++ Final_project.py
@@ -140,7 +140,7 @@ viz.link(moon, moonlight)
 
 # Make a lantern on a tree have some light because its dark at night
 lantern = forest.getChild('lantern')
-lantern_position = [-8.2, 1.08, -3]
+lantern_position = [-8.2, 1.10, -3]
 lantern.setPosition( lantern_position )
 
 #Add a light source to put inside the lantern.
@@ -239,7 +239,7 @@ vizact.onkeydown('e', viz.window.stopRecording )
 
 #Hit the 's' key for a screen capture. 
 #Save that files as 'image.bmp'. 
-vizact.onkeydown( 's', viz.window.screenCapture, 'image.bmp' )
+vizact.onkeydown( 's', viz.window.screenCapture, 'Vizard_screenShot.bmp' )
 
 
 # Grab the main view.
@@ -283,3 +283,121 @@ viz.phys.enable()
 ground = forest.getChild('Plane01')
 ground.collidePlane()
 a.collideBox()
+
+#
+#
+# Start Ben's Code. This is a pseudo random woods.
+for x in [7,-5, -3, 1,3, 5,7, 9]:
+	#for z in [-9, -6, -2, 2, 6, 10]:
+	z = (23*x +13)%10 + 10
+	myTrunk = vizshape.addCylinder(height = 2.0, radius = 0.5)
+	myTrunk.setPosition([x,1.0,z])
+	myTrunk.color([0.545,0.271,0.075])
+	myLeaves = vizshape.addCone(height = 3.0, radius = 1.5)
+	myLeaves.color([0,1,0])
+	myLeaves.setPosition([x,3.5,z])
+	
+#Dirt
+viz.startLayer(viz.POLYGON)
+viz.vertexColor(0.263, 0.078, 0.078)
+viz.vertex(-5, 0.1, -5)
+viz.vertex(-3, 0.1, 0)
+viz.vertex(2, 0.1, 0)
+viz.vertex(4, 0.1, 2)
+viz.vertex(0, 0.1, -5)
+myPath = viz.endLayer()
+myPath.setPosition(5, 0.1, 0)
+myPath.setScale([2,2,2])
+
+#River
+viz.startLayer(viz.TRIANGLE_FAN)
+viz.vertexColor(viz.BLUE)
+viz.vertex(-5, 0.1, -20)
+viz.vertex(-5, 0.1, -5)
+viz.vertex(-3, 0.1, 0)
+viz.vertex(2, 0.1, 0)
+viz.vertex(1, 0.1, 10)
+viz.vertexColor(0, 0, 0.8, 1)
+viz.vertex(-1, 0.1, 20)
+viz.vertex(3, 0.1, 7)
+viz.vertex(3, 0.1, 0)
+myStream = viz.endLayer()
+myStream.setPosition(-15, 0.1, 0)
+
+#
+#
+#Winnie Srart add a light
+
+#mylight = viz.addLight() 
+#mylight.enable() 
+#mylight.position(0, 1, 0) 
+#mylight.spread(180) 
+#mylight.intensity(2)
+
+#def AddLight(lightColor,quadColor,group):
+#
+#    light = viz.addPointLight(group=group)
+#    light.color(lightColor)
+#    light.linearattenuation(0.2)
+#
+#    LightQuad = viz.addTexQuad(parent=light, scale=[0.2]*3)
+#    LightQuad.billboard()
+#    LightQuad.color(quadColor)
+#    LightQuad.disable(viz.LIGHTING)
+#
+#    return light
+#
+#LIGHT_SPEED = 1
+#light_intense = 0.1
+#
+##Add red light
+#light1 = AddLight([0.5,0,0],viz.RED,5)
+#light1.intensity(light_intense)
+#light1.add(vizact.sequence(vizact.goto(-0.5,1.8,3,LIGHT_SPEED),vizact.goto(-0.5,1.8,-3,LIGHT_SPEED),viz.FOREVER))
+#
+##Add blue light
+#light2 = AddLight([0,0,0.5],viz.BLUE,0)
+#light2.intensity(light_intense)
+#light2.add(vizact.sequence(vizact.goto(0.5,1.8,-3,LIGHT_SPEED),vizact.goto(0.5,1.8,3,LIGHT_SPEED),viz.FOREVER))
+#
+##Add green light
+#light4 = AddLight([0,0.5,0],viz.GREEN,1)
+#light4.intensity(light_intense)
+#light4.add(vizact.sequence(vizact.goto(-0.5,1.8,-3,LIGHT_SPEED),vizact.goto(-0.5,1.8,3,LIGHT_SPEED),viz.FOREVER))
+#
+##Add yellow light
+#light5 = AddLight([1,1,0],viz.YELLOW,1)
+#light5.intensity(light_intense)
+#light5.add(vizact.sequence(vizact.goto(0.5,1.8,3,LIGHT_SPEED),vizact.goto(0.5,1.8,-3,LIGHT_SPEED),viz.FOREVER))
+#
+# Create spot light with texture attached 
+#stex = viz.addTexture('smiley.png')
+#light = vizfx.addSpotLight(texture=tex, color=viz.WHITE)
+
+duck = viz.add('duck.cfg')
+duck.setPosition([0,.5,4])
+duck.setEuler([180,0,0])
+
+#def getGesture():
+#    gesture = int(sensor.get()[-1])
+#    gestureText.message(gestureName[gesture])
+#
+#    if gesture == 2:
+#        #Middle finger point
+#        duck.setEuler([1,0,0],viz.REL_PARENT)
+#    if gesture == 8:
+#        #Little finger point
+#        duck.state(1)
+#    if gesture == 9:
+#        #Index and little finger point
+#        duck.state(2)
+#    if gesture == 1:
+#        #Index finger point
+#        duck.setPosition([0,.05,0], viz.REL_PARENT)
+#    if gesture == 0:
+#        #Fist
+#        duck.setPosition([0,-.05,0], viz.REL_PARENT)
+#
+#vizact.ontimer(0, getGesture)
+
+#Winnie End
